79ac73324c49d2da95f7d3228335217b92c55986
Mani Wadhwa <maniwadhwa26@gmail.com>
2019-02-25 23:57:35 +0530

    Custom Error Page

--- pages/_error.js
+++ pages/_error.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
-const indexPage = () => (
+const errorPage = () => (
     <div>
-        <h1>The Main Page</h1>
+        <h1>Something went wrong</h1>
         <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth') }>Go to Auth</button>
     </div>
 );
 
-export default indexPage;
+export default errorPage;
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import User from '../../components/User';
 const authIndexPage = () => (
     <div>
         <h1>The Auth Index Page</h1>
+        <User name="max" age={28}/>
     </div>
 );
 
